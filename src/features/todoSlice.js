f081cdd664dfbde4a531c0e9beb0fb5b30dfc462
picter <picter2esolv@gmail.com>
2023-04-04 15:03:08 -0400

    first commit

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -3,7 +3,6 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     items: []
 }
-
 export const todoSlice = createSlice({
     name: 'todos',
     initialState,
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
 import todoReducer from './features/todoSlice'
 
+
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
